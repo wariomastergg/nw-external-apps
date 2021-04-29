1c6cd35f45aea166df4a1f1913151f4093548479
colonel <71892575+wariomastergg@users.noreply.github.com>
2021-04-28 20:04:36 -0500

    Add files via upload

--- apps/apps.js
+++ apps/apps.js
@@ -2,11 +2,13 @@
 
 angular.module('nwas').service('apps', function() {
   return [
+    { name: "raycast", description: {en: "3d sage raycast port", fr: "3d sage raycast port"} },
+    { name: "BadApple", description: {en: "bad apple", fr: "bad apple"} },
     { name: "KhiCAS", description: {en: "Computer algebra system", fr: "Système de calcul formel"} },
     { name: "Periodic", description: {en: "Periodic table of elements", fr: "Tableau périodique des éléments"} },
     { name: "Nofrendo", description: {en: "NES emulator", fr: "Émulateur NES"} },
     { name: "Peanut-GB", description: {en: "GameBoy emulator", fr: "Émulateur GameBoy"} },
     { name: "HexEdit", description: {en: "Hexadecimal editor", fr: "Éditeur hexadécimal"} },
-    { name: "BadApple", description: {en: "baddest apple", fr: "giorino giovanna"} },
+    { name: "calcboy", description: {en: "gbc", fr: "gbc"} },
   ];
 });
